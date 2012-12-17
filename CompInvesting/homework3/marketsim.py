c31286a441823b510010a632c162b58fda0d61b6
Monstercoder <Monstercoder@gmail.com>
2012-12-16 22:00:43 -0600

    finish quiz4

--- CompInvesting/homework3/marketsim.py
+++ CompInvesting/homework3/marketsim.py
@@ -1,7 +1,17 @@
 import sys
 import csv
+import pandas 
+from qstkutil import DataAccess as da
+import numpy as np
+import math
+import copy
+import qstkutil.qsdateutil as du
+import datetime as dt
+import qstkutil.DataAccess as da
+import qstkutil.tsutil as tsu
+import qstkstudy.EventProfiler as ep
 
-cash= sys.argv[1]
+cash= float(sys.argv[1])
 orders =sys.argv[2]
 values = sys.argv[3]
 
@@ -9,9 +19,74 @@ print "initial cash:", cash
 print "orders: ", orders
 print "values: ", values
 
+# Reading the Data for the list of Symbols.	
+timeofday=dt.timedelta(hours=16)
+dataobj = da.DataAccess('Yahoo')
+#timestamps = du.getNYSEdays(startday,endday,timeofday)
+
+trades=[]
+symbols=[]
+holdings=[]
 of = open(orders, "rU")
 rd = csv.reader(of)
+
 for row in rd:
-	for col in row:
-		print col
+	trades.append([int(row[0]), int(row[1]), int(row[2]), row[3], row[4], int(row[5])])
+	if (row[3] not in symbols):
+		symbols.append(row[3])
 of.close()
+
+trades.sort(key=lambda d: d[0]*333+d[1]*222+d[2])
+
+length = len(trades) -1
+startday = dt.datetime(trades[0][0], trades[0][1], trades[0][2])
+endday = dt.datetime(trades[length][0], trades[length][1], trades[length][2],16,0)
+
+# Reading the Data for the list of Symbols.	
+timeofday=dt.timedelta(hours=16)
+timestamps = du.getNYSEdays(startday,endday,timeofday)
+dataobj = da.DataAccess('Yahoo')
+#print timestamps
+close = dataobj.get_data(timestamps, symbols, "close")
+
+def calholdings(timestamp,holdings, close):
+	value = 0
+	for holding in holdings:
+		value = value + close[holding[0]][timestamp] * holding[1]
+	return value
+	
+
+currenttimestamp =dt.datetime(trades[0][0], trades[0][1], trades[0][2], 16, 0) 
+currentvalue = cash
+
+for trade in trades:
+	sym = trade[3]
+	timestamp = dt.datetime(trade[0], trade[1], trade[2], 16,0)
+	cost=close[sym][timestamp] * trade[5]
+	if (currenttimestamp==dt.datetime(2011,02,10, 16, 0)) and timestamp != currenttimestamp:
+		print dt.datetime(2011,02,18, 16, 0), cash + calholdings(dt.datetime(2011,02,18, 16, 0), holdings, close)
+
+	if (trade[4]=="Buy"):
+		cash= cash - cost
+		holdings.append([sym,trade[5]])
+	else:
+		cash = cash + cost
+		holdings.append([sym, trade[5] * -1])	
+	#print trade, "cost ", cost, "cash: ", cash
+
+	#if (currenttimestamp != timestamp):
+	#	print currenttimestamp, " ", currentvalue
+
+	currenttimestamp = timestamp
+	currentvalue= cash + calholdings(timestamp, holdings, close)
+
+#print currenttimestamp, " ", currentvalue
+
+		
+	#price = close[trade[3]][]
+#	print "$",sym, timestamp,close[sym][timestamp]
+
+
+
+
+
